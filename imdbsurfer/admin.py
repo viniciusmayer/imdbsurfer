52167c7578f4ae1d397f3e46e72945d87653643c
Eleonor Vinicius <viniciusmayer@gmail.com>
2017-06-23 00:27:36 -0300

    Basic admin configuration for Movie entity

--- imdbsurfer/admin.py
+++ imdbsurfer/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 
+from models import Movie
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'index', 'rate', 'votes', 'link', 'obs')
+    list_filter = ['year', 'index', 'rate']
+    search_fields = ['name', 'obs']
+    
+    def save_model(self, request, obj, form, change):
+        obj.user_create = request.user
+        obj.user_update = request.user
+        super(MovieAdmin, self).save_model(request, obj, form, change)
+
+admin.site.register(Movie, MovieAdmin)
--- imdbsurfer/models.py
+++ imdbsurfer/models.py
@@ -6,6 +6,6 @@ from common.models import CommonInfo
 class Movie(CommonInfo):
     year = models.PositiveSmallIntegerField()
     index = models.PositiveSmallIntegerField()
-    rate = models.DecimalField(max_digits=2, decimal_places=1)
+    rate = models.DecimalField(max_digits=3, decimal_places=1)
     votes = models.PositiveIntegerField()
     link = models.CharField(max_length=255)
