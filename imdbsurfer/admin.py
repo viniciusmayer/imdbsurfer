862d164463e607d615c35e987d20aa1e5e6d65c1
Eleonor Vinicius <viniciusmayer@gmail.com>
2017-06-24 18:09:06 -0300

    Base modeling done

--- imdbsurfer/admin.py
+++ imdbsurfer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Movie
+from models import Movie, Role, Artist, Genre, ArtistRole
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('name', 'year', 'index', 'rate', 'metascore', 'minutes', 'votes', 'link', 'obs')
@@ -13,3 +13,36 @@ class MovieAdmin(admin.ModelAdmin):
         super(MovieAdmin, self).save_model(request, obj, form, change)
 
 admin.site.register(Movie, MovieAdmin)
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'obs')
+    search_fields = ['name', 'obs']
+    
+    def save_model(self, request, obj, form, change):
+        obj.user_create = request.user
+        obj.user_update = request.user
+        super(RoleAdmin, self).save_model(request, obj, form, change)
+        
+admin.site.register(Role, RoleAdmin)
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'obs')
+    search_fields = ['name', 'obs']
+    
+    def save_model(self, request, obj, form, change):
+        obj.user_create = request.user
+        obj.user_update = request.user
+        super(ArtistAdmin, self).save_model(request, obj, form, change)
+        
+admin.site.register(Artist, ArtistAdmin)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'obs')
+    search_fields = ['name', 'obs']
+    
+    def save_model(self, request, obj, form, change):
+        obj.user_create = request.user
+        obj.user_update = request.user
+        super(GenreAdmin, self).save_model(request, obj, form, change)
+
+admin.site.register(Genre, GenreAdmin)
--- imdbsurfer/models.py
+++ imdbsurfer/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 
-from common.models import CommonInfo 
+from common.models import CommonInfo, Common 
 
+    
+class Role(CommonInfo):
+    def __str__(self):
+        return self.name
+
+class Artist(CommonInfo):
+    role = models.ManyToManyField(Role, through='ArtistRole')
+
+    def __str__(self):
+        return self.name
+    
+class Genre(CommonInfo):
+    def __str__(self):
+        return self.name
+
+class ArtistRole(Common):
+    artist = models.ForeignKey(Artist)
+    role = models.ForeignKey(Role)
 
 class Movie(CommonInfo):
     year = models.PositiveSmallIntegerField()
@@ -11,3 +29,18 @@ class Movie(CommonInfo):
     link = models.CharField(max_length=255)
     minutes = models.PositiveSmallIntegerField()
     metascore = models.PositiveSmallIntegerField(null=True)
+    watched = models.BooleanField(default=False)
+    watch = models.BooleanField(default=False)
+    genres = models.ManyToManyField(Genre, through='MovieGenre')
+    artists = models.ManyToManyField(ArtistRole, through='MovieArtistRole')
+    
+    def __str__(self):
+        return '{0} ({1})'.format(self.name, self.year) 
+
+class MovieGenre(Common):
+    movie = models.ForeignKey(Movie)
+    genre = models.ForeignKey(Genre)
+
+class MovieArtistRole(Common):
+    movie = models.ForeignKey(Movie)
+    artistRole = models.ForeignKey(ArtistRole)
